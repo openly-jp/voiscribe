1a44956b13d21c3abb46e0cc769ac99a89535f8c
Yasutaka Odo <YasutakaOdo@gmail.com>
2022-12-14 00:08:15 +0900

    Initial Commit

---
+++ whisper-ios/whisper_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  whisper_iosApp.swift
+//  whisper-ios
+//
+//  Created by Yasutaka Odo on 2022/12/14.
+//
+
+import SwiftUI
+
+@main
+struct whisper_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
