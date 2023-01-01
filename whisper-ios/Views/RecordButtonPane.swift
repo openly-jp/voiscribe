84f0e34f57828741703786daa025863664646291
ooyamatakehisa <ooyamatakehisa@gmail.com>
2023-01-01 19:31:36 +0900

    add recognition pane and display waveform and recording time

--- whisper-ios/Views/RecordButtonPane.swift
+++ whisper-ios/Views/RecordButtonPane.swift
@@ -7,12 +7,14 @@
 //
 
 import SwiftUI
+import AVFoundation
+
 
 struct RecordButtonPane: View {
     @Binding var isActive: Bool
-    @State var startAction = { }
-    @State var stopAction = { }
-
+    let startAction: ()->Void
+    let stopAction: ()->Void
+    
     @State var buttonColor: Color = .red
     @State var borderStrokeColor: Color = .red
     @State var borderStrokeWidth: CGFloat = 2
@@ -20,48 +22,40 @@ struct RecordButtonPane: View {
     @State var animation: Animation = .easeInOut
     @State var stoppedStateCornerRadius: CGFloat = 0.10
     @State var stoppedStateSize: CGFloat = 0.5
-
-
+    
     var body: some View {
         ZStack {
             Circle()
                 .stroke(borderStrokeColor, lineWidth: borderStrokeWidth)
                 .frame(width: 100, height: 100)
-
+            
             recordButton(size: 100 - borderSpacing)
                 .animation(animation)
                 .foregroundColor(buttonColor)
         }
     }
-
-    func activate() {
-        isActive = true
-        startAction()
-    }
-
-    func deactivate() {
-        isActive = false
-        stopAction()
-    }
-
+    
+    
     private func recordButton(size: CGFloat) -> some View {
         if !isActive {
-            return Button(action: { activate() }) {
-                RoundedRectangle(cornerRadius: size)
-                    .frame(width: size, height: size)
-            }
+            return Button(action: startAction){ RoundedRectangle(cornerRadius: size)
+                .frame(width: size, height: size)
+                   }
+            
         } else {
-            return Button(action: { deactivate() }) {
+            return Button(action: stopAction){
                 RoundedRectangle(cornerRadius: size * stoppedStateCornerRadius)
                     .frame(width: size * stoppedStateSize, height: size * stoppedStateSize)
             }
+            
         }
     }
 }
 
 struct RecordButton_Previews: PreviewProvider {
+    
     static var previews: some View {
-        RecordButtonPane(isActive: .constant(false))
+        return RecordButtonPane(isActive: .constant(false), startAction: {}, stopAction: {})
     }
 }
 
---
+++ whisper-ios/Views/Waveform.swift
@@ -0,0 +1,51 @@
+//
+//  Waveform.swift
+//  whisper-ios
+//
+//  Created by creevo on 2023/01/01.
+//  Copyright © 2023 jp.openly. All rights reserved.
+//
+
+import SwiftUI
+
+struct IdAmp : Identifiable {
+    var id: UUID
+    var amp: Float
+    var idx: Int
+}
+
+struct Waveform: View {
+    @Binding var idAmps: [IdAmp]
+    
+    let e: Float = 2.718281828459
+    let rectangleWidth: CGFloat = 2.0
+    let spacingWidth: CGFloat = 2.0
+
+    var body: some View {
+        GeometryReader { geometry in
+            Group {
+                ForEach(idAmps) { idAmp in
+                    let amp = CGFloat(powf(e, idAmp.amp / 10))
+
+                    let numRightRecs = CGFloat(idAmps.count - 1 - idAmp.idx)
+                    let rightX = numRightRecs * (rectangleWidth + spacingWidth)
+                    Rectangle()
+                        .frame(width: rectangleWidth, height: amp * 50)
+                        .position(x: geometry.size.width - rightX)
+                }
+            }
+        }
+    }
+}
+
+struct Waveform_Previews: PreviewProvider {
+    static var previews: some View {
+        var idAmps: [IdAmp] = []
+        var idx = 0
+        for amp in [-50, -2, -14, -14, -11, -42, -21, -100, -14] {
+            idAmps.append(IdAmp(id: UUID(), amp: Float(amp), idx: idx))
+            idx += 1
+        }
+        return Waveform(idAmps: .constant(idAmps))
+    }
+}
