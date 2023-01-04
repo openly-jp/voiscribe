bcfc416c56e93312b3a552b604556a68c77f4557
ooyamatakehisa <ooyamatakehisa@gmail.com>
2023-01-04 17:32:20 +0900

    hide record button on detail screen

--- whisper-ios/Views/RecordDetails.swift
+++ whisper-ios/Views/RecordDetails.swift
@@ -47,9 +47,10 @@ struct RecordDetails: View {
                         }
                         Divider()
                     }
-                }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
-                    .padding()
-                    .navigationBarTitle("", displayMode: .inline)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                .padding()
+                .navigationBarTitle("", displayMode: .inline)
             }
         }
     }
