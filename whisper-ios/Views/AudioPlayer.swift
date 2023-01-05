ac48030a3ff2df860902a12364bfd8fc8176ab96
Takehisa Oyama <44559556+ooyamatakehisa@users.noreply.github.com>
2023-01-05 15:01:15 +0900

    implement audio player (#27)
    * hide record button on detail screen
    
    * finish implementing audio player
    
    * change button size
    
    * fix preview is not displayed correctly
    
    * fix screen while recognition
    
    * auto highlight
    
    * enable to change speed

---
+++ whisper-ios/Views/AudioPlayer.swift
@@ -0,0 +1,139 @@
+//import UIKit
+import AVFoundation
+import SwiftUI
+
+let availableSpeedRates = [0.5, 0.8, 1, 1.2, 1.5, 2, 2.5, 3.5, 4]
+
+
+struct AudioPlayer: View {
+    @Binding var player: AVAudioPlayer
+    @Binding var currentPlayingTime: Double
+    @State var isEditing = false
+    @State var isPlaying = false
+
+    @State var isChangingSpeedRate = false
+    @State var speedRateIdx = 2 // speedRate = 1x
+    
+    @State var updateRecordingTimeTimer: Timer? = nil
+
+    var body: some View {
+        VStack(spacing: 10) {
+            Slider(value: $currentPlayingTime, in: 0...player.duration) { editing in
+                isEditing = editing
+                if !editing {
+                    player.currentTime = currentPlayingTime
+                }
+            }
+
+            HStack {
+                Text(formatTime(player.currentTime, duration: player.duration))
+                Spacer()
+                Text(formatTime(player.duration - player.currentTime, duration: player.duration))
+            }
+            .font(.caption)
+
+            HStack {
+                Button(speedRate2String(availableSpeedRates[speedRateIdx])) { isChangingSpeedRate = true }
+                    .foregroundColor(Color(.secondaryLabel))
+                    .sheet(isPresented: $isChangingSpeedRate) { changeSpeedSheetView }
+                Spacer()
+                PlayerButton(name: "gobackward.5", size: 35) { player.currentTime -= 5 }
+                Spacer()
+                PlayerButton(
+                    name: isPlaying ? "pause.circle.fill" : "play.circle.fill",
+                    size: 55,
+                    action: playOrPause
+                )
+                Spacer()
+                PlayerButton(name: "goforward.5", size: 35) { player.currentTime += 5 }
+                Spacer()
+                
+                // the following component is only for aligning components equally
+                Button("1x"){}.hidden()
+            }
+            .padding(.horizontal, 30)
+            .padding(.bottom, 10)
+        }.onDisappear {
+            player.stop()
+            if let updateRecordingTimeTimer {
+                updateRecordingTimeTimer.invalidate()
+            }
+        }
+    }
+    
+    var changeSpeedSheetView: some View {
+        Group {
+            NavigationView {
+                List {
+                    ForEach(Array(availableSpeedRates.enumerated()), id: \.self.offset) { idx, speedRate in
+                        Button {
+                            speedRateIdx = idx
+                            player.rate = Float(speedRate)
+                        } label: {
+                            HStack {
+                                Text(speedRate2String(speedRate))
+                                Spacer()
+                                if idx == speedRateIdx {
+                                    Image(systemName: "checkmark")
+                                }
+                            }.padding(10)
+                        }
+                    }
+                }
+                .navigationBarTitle("再生速度の変更")
+                .listStyle(.inset)
+            }
+            HStack{
+                Spacer()
+                Button("閉じる") {
+                    isChangingSpeedRate = false
+                }
+                Spacer()
+            }
+        }
+    }
+    
+    func playOrPause() {
+        if !isPlaying {
+            updateRecordingTimeTimer = Timer.scheduledTimer(
+                withTimeInterval: 0.1,
+                repeats: true
+            ) { _ in
+                if !isEditing {
+                    currentPlayingTime = player.currentTime
+                }
+            }
+            player.play()
+        } else {
+            player.pause()
+        }
+        isPlaying = !isPlaying
+    }
+    
+    func speedRate2String(_ speedRate: Double) -> String {
+        return "\(String(format: "%g", speedRate))x"
+    }
+}
+
+struct PlayerButton: View {
+    let name: String
+    let size: Int
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            Image(systemName: name)
+                .font(.system(size: CGFloat(size)))
+                .foregroundColor(Color(.secondaryLabel))
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        let url = getRecognizedSpeechMock(audioFileName: "sample_ja", csvFileName: "sample_ja")?.audioFileURL
+        let player = try! AVAudioPlayer(contentsOf: url!)
+        AudioPlayer(player: .constant(player), currentPlayingTime: .constant(0))
+    }
+}
--- whisper-ios/Views/RecordDetails.swift
+++ whisper-ios/Views/RecordDetails.swift
@@ -1,57 +1,102 @@
+import AVFoundation
 import SwiftUI
 
 struct RecordDetails: View {
     let recognizedSpeech: RecognizedSpeech
     let isRecognizing: Bool
-    let recognizedSpeech2: RecognizedSpeech! = getRecognizedSpeechMock(audioFileName: "sample_ja", csvFileName: "sample_ja")
     func getLocaleDateString(date: Date) -> String{
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "ja_JP")
         dateFormatter.dateStyle = .medium
         dateFormatter.dateFormat = "yyyy年MM月dd日 HH:mm"
-        
+
         return dateFormatter.string(from: date)
     }
-    func getStartTimeStringFromMsec(startMsec: Int64) -> String {
-        let min = startMsec / 1000 / 60
-        let sec = startMsec / 1000 - (min * 60)
-        return String(format: "%02d:%02d", min, sec)
+
+    // MARK: - state about player
+    @State var player: AVAudioPlayer
+    @State var currentPlayingTime: Double = 0
+    @State var currentLineIdx: Int = 0
+
+    init(
+        recognizedSpeech: RecognizedSpeech,
+        isRecognizing: Bool
+    ) {
+        self.recognizedSpeech = recognizedSpeech
+        self.isRecognizing = isRecognizing
+
+        // TODO: fix this (issue #25)
+        let url = getURLByName(fileName: recognizedSpeech.audioFileURL.lastPathComponent)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.enableRate = true
+        } catch {
+            player = try! AVAudioPlayer()
+            debugPrint("fail to init audio player")
+        }
     }
+
     var body: some View {
-        if isRecognizing {
-            Text("認識中")
-        } else {
-            VStack(alignment: .leading){
-                Text(getLocaleDateString(date: recognizedSpeech.createdAt))
-                    .foregroundColor(Color.gray)
-                    .padding(.horizontal)
-                Text(recognizedSpeech.title)
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .padding(.horizontal)
-                Rectangle()
-                    .frame(height: 2)
-                    .foregroundColor(Color.gray)
-                    .padding(.horizontal)
+        return VStack(alignment: .leading){
+            Text(getLocaleDateString(date: recognizedSpeech.createdAt))
+                .foregroundColor(Color.gray)
+                .padding(.horizontal)
+            Text(recognizedSpeech.title)
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.horizontal)
+            Rectangle()
+                .frame(height: 2)
+                .foregroundColor(Color.gray)
+                .padding(.horizontal)
+            if isRecognizing {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Text("認識中")
+                    Spacer()
+                }
+                Spacer()
+            } else {
                 ScrollView{
-                    ForEach(recognizedSpeech.transcriptionLines) {
-                        transcriptionLine in
-                        HStack(alignment: .center){
-                            Text(getStartTimeStringFromMsec(startMsec: transcriptionLine.startMSec))
-                                .frame(width: 50, alignment: .center)
-                                .foregroundColor(Color.blue)
-                                .padding()
-                            Spacer()
-                            Text(transcriptionLine.text)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                        }
-                        Divider()
+                    ForEach(Array(recognizedSpeech.transcriptionLines.enumerated()), id: \.self.offset) {
+                        idx, transcriptionLine in
+                        Group{
+                            HStack(alignment: .center){
+                                Button {
+                                    player.currentTime = Double(transcriptionLine.startMSec) / 1000
+                                    currentPlayingTime = Double(transcriptionLine.startMSec) / 1000
+                                } label: {
+                                    Text(formatTime(Double(transcriptionLine.startMSec) / 1000))
+                                        .frame(width: 50, alignment: .center)
+                                        .foregroundColor(Color.blue)
+                                        .padding()
+                                }
+                                Spacer()
+                                Text(transcriptionLine.text)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                            }
+                           Divider()
+                        }.background(getTextColor(idx))
                     }
-                }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
-                    .padding()
-                    .navigationBarTitle("", displayMode: .inline)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                .padding()
+                .navigationBarTitle("", displayMode: .inline)
+                AudioPlayer(player: $player, currentPlayingTime: $currentPlayingTime)
+                    .padding(20)
             }
         }
+    }
+
+    func getTextColor(_ idx: Int) -> Color {
+        let lines = recognizedSpeech.transcriptionLines
+        let startMSec = Double(lines[idx].startMSec)
+        let endMSec: Double = idx < lines.count - 1 ? Double(lines[idx + 1].startMSec) : .infinity
+        let currentMSec = currentPlayingTime * 1000
+        let isInside = startMSec <= currentMSec && currentMSec < endMSec
+        let uiColor: UIColor = isInside ? .systemGray5 : .systemBackground
+        return Color(uiColor)
     }
 }
 
