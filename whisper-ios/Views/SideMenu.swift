068be53e8c9739361abc7e21421a1c021be1a202
shibukazu <61775791+shibukazu@users.noreply.github.com>
2023-02-20 02:17:49 +0900

    Activate swipe gesture for side menu closing (#181)
    * add smooth sidemenu close feature
    
    * fix to activate record button
    
    * format

--- whisper-ios/Views/SideMenu.swift
+++ whisper-ios/Views/SideMenu.swift
@@ -1,8 +1,12 @@
 import SwiftUI
 
+let sideMenuWidth: CGFloat = 270
+let sideMenuOpenOffset: CGFloat = 0
+let sideMenuCloseOffset: CGFloat = -1 * sideMenuWidth
+
 struct SideMenu: View {
     @Binding var isOpen: Bool
-    let width: CGFloat = 270
+    @Binding var offset: CGFloat
     let menuItems: [MenuItem] = [
         modelLoadMenuItem,
         recognitionFrequencySecMenuItem,
@@ -12,25 +16,44 @@ struct SideMenu: View {
     ]
 
     var body: some View {
-        ZStack {
-            GeometryReader { _ in
-                EmptyView()
-            }
-            .background(Color.gray.opacity(0.3))
-            .opacity(self.isOpen ? 1.0 : 0.0)
-            .animation(.easeIn(duration: 0.25))
-            .onTapGesture(perform: {
-                self.isOpen = false
-            })
-            HStack {
-                List(menuItems, children: \.subMenuItems) {
-                    item in item.view
+        GeometryReader {
+            _ in
+            ZStack {
+                GeometryReader { _ in
+                    EmptyView()
                 }
-                .frame(width: width)
-                .offset(x: self.isOpen ? 0 : -self.width)
-                .animation(.easeIn(duration: 0.25))
-                Spacer()
+                // background and opacity is needed for activating onTapGesture
+                .background(Color.gray.opacity(0.1))
+                .opacity(isOpen ? 1 : 0)
+                .onTapGesture(perform: {
+                    offset = sideMenuCloseOffset
+                    isOpen = false
+                })
+                HStack {
+                    List(menuItems, children: \.subMenuItems) {
+                        item in item.view
+                    }
+                    .frame(width: sideMenuWidth)
+                    .offset(x: offset)
+                    .animation(.default)
+                    Spacer()
+                }
             }
+            .gesture(isOpen ? DragGesture()
+                .onChanged {
+                    value in
+                    if value.translation.width < 0 {
+                        offset = max(sideMenuCloseOffset, sideMenuOpenOffset - abs(value.translation.width))
+                    }
+                }
+                .onEnded { _ in
+                    if offset < 0.5 * sideMenuCloseOffset {
+                        offset = sideMenuCloseOffset
+                        isOpen = false
+                    } else {
+                        offset = sideMenuOpenOffset
+                    }
+                } : nil)
         }
     }
 }
@@ -42,9 +65,9 @@ struct MenuItem: Identifiable {
 }
 
 struct SideMenu_Previews: PreviewProvider {
-    @State static var page = 0
     @State static var isOpen = true
+    @State static var offset: CGFloat = 0
     static var previews: some View {
-        SideMenu(isOpen: $isOpen)
+        SideMenu(isOpen: $isOpen, offset: $offset)
     }
 }
