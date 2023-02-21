8560c48527c89e5ed5afdf8b773e32533db7cf1a
Hwari2929 <125178982+Hwari2929@users.noreply.github.com>
2023-02-21 22:06:04 +0900

    splash-screen

--- whisper-ios/whisper_iosApp.swift
+++ whisper-ios/whisper_iosApp.swift
@@ -18,24 +18,32 @@ struct StartView: View {
 
     var body: some View {
         if isLoading {
-            Image("icon")
-                .resizable()
-                .frame(width: 60, height: 60)
-                .onAppear {
-                    DispatchQueue.global(qos: .userInteractive).async {
-                        let whisperModel = WhisperModel(
-                            size: defaultModelSize,
-                            language: defaultModelLanguage
-                        )
-                        whisperModel.loadModel { err in
-                            if let err { Logger.error(err); return }
+            ZStack {
+                VStack {
+                    Image("loadingscreen")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 2048)
+                        .onAppear {
+                            DispatchQueue.global(qos: .userInteractive).async {
+                            let whisperModel = WhisperModel(
+                                size: defaultModelSize,
+                                language: defaultModelLanguage
+                            )
+                            whisperModel.loadModel { err in
+                                if let err { Logger.error(err); return }
 
-                            recognizer = try! WhisperRecognizer(whisperModel: whisperModel)
-                            isLoading = false
+                                recognizer = try! WhisperRecognizer(whisperModel: whisperModel)
+                                isLoading = false
+                            }
                         }
                     }
                 }
-            Text(APP_NAME)
+                VStack {
+                    Text(APP_NAME)
+                        .foregroundColor(.white)
+                }
+            }
         } else {
             HomeView()
                 .environmentObject(recognizer!)
