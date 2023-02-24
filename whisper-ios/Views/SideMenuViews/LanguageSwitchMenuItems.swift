78d769599ae1349bc0449c92353745c8d9e5cccb
Yasutaka Odo <YasutakaOdo@gmail.com>
2023-02-24 23:32:47 +0900

    hide switch item when there is only one display language installed on device

--- whisper-ios/Views/SideMenuViews/LanguageSwitchMenuItems.swift
+++ whisper-ios/Views/SideMenuViews/LanguageSwitchMenuItems.swift
@@ -3,43 +3,55 @@ import SwiftUI
 struct LanguageSwitchItemView: View {
     @State private var showingiOSAlert = false
     @State private var showingAlert = false
+
     var body: some View {
-        VStack {
-            Button(action: {
-                showingAlert = true
-                if !isRunningOnMacOS() {
-                    self.showingiOSAlert = true
+        Group {
+            ///  We need to get unique language code.
+            ///  let localeIdentifiers = ["en-US", "en-GB", "fr-FR", "ja-JP", "ja-US", "es-ES"]
+            ///  let uniqueLanguageCodes = Set(localeIdentifiers.map { Locale(identifier: $0).languageCode })
+            ///  print(uniqueLanguageCodes)
+            ///   Output: ["en", "fr", "ja", "es"]
+            let uniqueLanguageCodes = Set(NSLocale.preferredLanguages.map { Locale(identifier: $0).languageCode })
+
+            if uniqueLanguageCodes.count > 1 {
+                Button(action: {
+                    print(NSLocale.preferredLanguages)
+                    print(uniqueLanguageCodes)
+                    showingAlert = true
+                    if !isRunningOnMacOS() {
+                        self.showingiOSAlert = true
+                    }
+                }) {
+                    HStack {
+                        Image(systemName: "globe")
+                            .imageScale(.large)
+                            .frame(width: 32)
+                            .tint(Color(.label))
+                        Text(NSLocalizedString("表示言語", comment: ""))
+                            .font(.headline)
+                            .tint(Color(.label))
+                        Spacer()
+                    }
                 }
-            }) {
-                HStack {
-                    Image(systemName: "globe")
-                        .imageScale(.large)
-                        .frame(width: 32)
-                        .tint(Color(.label))
-                    Text(NSLocalizedString("表示言語", comment: ""))
-                        .font(.headline)
-                        .tint(Color(.label))
-                    Spacer()
+                .alert(isPresented: $showingAlert) {
+                    showingiOSAlert
+                        ? Alert(
+                            title: Text(NSLocalizedString("警告", comment: "")),
+                            message: Text(NSLocalizedString("言語選択の警告", comment: "")),
+                            primaryButton: .default(Text(NSLocalizedString("閉じる", comment: ""))),
+                            secondaryButton: .default(Text(NSLocalizedString("開く", comment: "")), action: {
+                                if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
+                                    UIApplication.shared.open(settingsURL, options: [:], completionHandler: nil)
+                                }
+                            })
+                        )
+                        : Alert(
+                            title: Text(NSLocalizedString("警告", comment: "")),
+                            message: Text(NSLocalizedString("macでの言語選択の警告", comment: "")),
+                            dismissButton: .default(Text(NSLocalizedString("閉じる", comment: "")))
+                        )
                 }
             }
-        }
-        .alert(isPresented: $showingAlert) {
-            showingiOSAlert
-                ? Alert(
-                    title: Text(NSLocalizedString("警告", comment: "")),
-                    message: Text(NSLocalizedString("言語選択の警告", comment: "")),
-                    primaryButton: .default(Text(NSLocalizedString("閉じる", comment: ""))),
-                    secondaryButton: .default(Text(NSLocalizedString("開く", comment: "")), action: {
-                        if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
-                            UIApplication.shared.open(settingsURL, options: [:], completionHandler: nil)
-                        }
-                    })
-                )
-                : Alert(
-                    title: Text(NSLocalizedString("警告", comment: "")),
-                    message: Text(NSLocalizedString("macでの言語選択の警告", comment: "")),
-                    dismissButton: .default(Text(NSLocalizedString("閉じる", comment: "")))
-                )
         }
     }
 
