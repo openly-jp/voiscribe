53e5a19a1369150fd352fde2081429316f8a4849
Yasutaka Odo <YasutakaOdo@gmail.com>
2023-02-27 07:21:42 +0900

    enable crash logging

--- whisper-ios/whisper_iosApp.swift
+++ whisper-ios/whisper_iosApp.swift
@@ -1,7 +1,18 @@
+import Firebase
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_: UIApplication,
+                     didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool
+    {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct WhisperTestApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             StartView()
