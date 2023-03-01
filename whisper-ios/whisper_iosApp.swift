e1ed69dc3cd60fabc3c32349d64c822d08a4a60c
Hwari2929 <125178982+Hwari2929@users.noreply.github.com>
2023-03-01 21:36:06 +0900

    remove white blank from splash-screen (#237)
    * remove white blank from splash-screen
    
    * non-use image delete
    
    * rename icon

--- whisper-ios/whisper_iosApp.swift
+++ whisper-ios/whisper_iosApp.swift
@@ -32,11 +32,24 @@ struct StartView: View {
     var body: some View {
         if isLoading {
             ZStack {
+                ZStack {
+                    LinearGradient(
+                        gradient: Gradient(colors: [
+                            Color(red: 0, green: 0.549, blue: 0.8352),
+                            Color(red: 0.15, green: 0.3333, blue: 0.6666),
+                            Color(red: 0.3725, green: 0.0901, blue: 0.3019),
+                        ]),
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                    .aspectRatio(contentMode: .fill)
+                    .ignoresSafeArea()
+                }
                 VStack {
-                    Image("loadingscreen")
+                    Image("iconwhite")
                         .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 2048)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 128)
                         .onAppear {
                             DispatchQueue.global(qos: .userInteractive).async {
                                 let whisperModel = WhisperModel(
@@ -51,8 +64,7 @@ struct StartView: View {
                                 }
                             }
                         }
-                }
-                VStack {
+                    Spacer().frame(height: 15)
                     Text(APP_NAME)
                         .foregroundColor(.white)
                 }
