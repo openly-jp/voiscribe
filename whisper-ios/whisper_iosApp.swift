01cda1382fb93e1b66eb995236f784e0b1dba206
Hwari2929 <125178982+Hwari2929@users.noreply.github.com>
2023-03-01 09:35:46 +0900

    commit

--- whisper-ios/whisper_iosApp.swift
+++ whisper-ios/whisper_iosApp.swift
@@ -12,18 +12,30 @@ struct WhisperTestApp: App {
 struct StartView: View {
     @State var isLoading: Bool = true
     @State var recognizer: WhisperRecognizer?
-
     @AppStorage(userDefaultModelSizeKey) var defaultModelSize = Size(rawValue: "tiny")!
     @AppStorage(userDefaultModelLanguageKey) var defaultModelLanguage = Lang(rawValue: "en")!
 
     var body: some View {
         if isLoading {
             ZStack {
+                ZStack {
+                    LinearGradient(
+                        gradient: Gradient(colors: [
+                            Color(red: 0, green: 0.549, blue: 0.8352),
+                            Color(red: 0.15, green: 0.3333, blue: 0.6666),
+                            Color(red: 0.3725, green: 0.0901, blue: 0.3019),
+                        ]),
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                    .aspectRatio(contentMode: .fill)
+                    .ignoresSafeArea()
+                }
                 VStack {
-                    Image("loadingscreen")
+                    Image("icononly")
                         .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 2048)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 128)
                         .onAppear {
                             DispatchQueue.global(qos: .userInteractive).async {
                                 let whisperModel = WhisperModel(
@@ -38,8 +50,7 @@ struct StartView: View {
                                 }
                             }
                         }
-                }
-                VStack {
+                    Spacer().frame(height: 15)
                     Text(APP_NAME)
                         .foregroundColor(.white)
                 }
