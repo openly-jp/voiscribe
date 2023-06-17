1f8ce86a0a3b6e7929a52eed5bda65440f8b8c0f
ooyamatakehisa <ooyamatakehisa@gmail.com>
2023-06-18 01:03:29 +0900

    extract the view of splash screen

--- whisper-ios/whisper_iosApp.swift
+++ whisper-ios/whisper_iosApp.swift
@@ -42,48 +42,52 @@ struct StartView: View {
 
     var body: some View {
         if isLoading {
-            ZStack {
-                ZStack {
-                    LinearGradient(
-                        gradient: Gradient(colors: [
-                            Color(red: 0, green: 0.549, blue: 0.8352),
-                            Color(red: 0.15, green: 0.3333, blue: 0.6666),
-                            Color(red: 0.3725, green: 0.0901, blue: 0.3019),
-                        ]),
-                        startPoint: .topLeading,
-                        endPoint: .bottomTrailing
-                    )
-                    .aspectRatio(contentMode: .fill)
-                    .ignoresSafeArea()
-                }
-                VStack {
-                    Image("iconwhite")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 128)
-                        .onAppear {
-                            DispatchQueue.global(qos: .userInteractive).async {
-                                let whisperModel = WhisperModel(
-                                    size: defaultModelSize,
-                                    recognitionLanguage: defaultRecognitionLanguage
-                                )
-                                whisperModel.loadModel { err in
-                                    if let err { Logger.error(err); return }
-
-                                    recognizer = try! WhisperRecognizer(whisperModel: whisperModel)
-                                    isLoading = false
-                                }
-                            }
-                        }
-                    Spacer().frame(height: 15)
-                    Text(APP_NAME)
-                        .foregroundColor(.white)
-                }
-            }
+            splashScreen
         } else {
             HomeView()
                 .environmentObject(recognizer!)
                 .attachPartialSheetToRoot()
+        }
+    }
+    
+    var splashScreen: some View {
+        ZStack {
+            ZStack {
+                LinearGradient(
+                    gradient: Gradient(colors: [
+                        Color(red: 0, green: 0.549, blue: 0.8352),
+                        Color(red: 0.15, green: 0.3333, blue: 0.6666),
+                        Color(red: 0.3725, green: 0.0901, blue: 0.3019),
+                    ]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
+            }
+            VStack {
+                Image("iconwhite")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 128)
+                    .onAppear {
+                        DispatchQueue.global(qos: .userInteractive).async {
+                            let whisperModel = WhisperModel(
+                                size: defaultModelSize,
+                                recognitionLanguage: defaultRecognitionLanguage
+                            )
+                            whisperModel.loadModel { err in
+                                if let err { Logger.error(err); return }
+
+                                recognizer = try! WhisperRecognizer(whisperModel: whisperModel)
+                                isLoading = false
+                            }
+                        }
+                    }
+                Spacer().frame(height: 15)
+                Text(APP_NAME)
+                    .foregroundColor(.white)
+            }
         }
     }
 }
